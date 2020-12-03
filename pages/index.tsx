720413ada5c9ffa1eadc44b29449f4c45e714818
Kevin King <4kevinking@gmail.com>
2020-12-03 14:03:35 -0800

    add Position class to presence

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
-import { useRoom } from "@roomservice/react";
+import { usePresence, useRoom } from "@roomservice/react";
+import { useEffect } from "react";
+
+interface Position {
+  x: number;
+  y: number;
+}
+
+const ROOM = "maze-global";
 
 export default function Home() {
-  const room = useRoom("global");
+  const room = useRoom(ROOM);
+
+  const [positions, positionClient] = usePresence<Position>(ROOM, "position");
+
+  useEffect(() => {
+    positionClient.set({ x: 0, y: 0 });
+  }, []);
 
   if (!room) {
     return <div>Loading...</div>;
