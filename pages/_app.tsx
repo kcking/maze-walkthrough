ded6fccbfbbc0e398b7edcd73343b387ff99e51d
Kevin King <4kevinking@gmail.com>
2020-12-30 19:04:12 -0800

    update client to use cookie

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import { RoomServiceProvider } from "@roomservice/react";
+import Cookies from "js-cookie";
+import { nanoid } from "nanoid";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (Cookies.get("roomservice-user") === undefined) {
+      Cookies.set("roomservice-user", nanoid());
+    }
+  }, []);
+
   return (
     <RoomServiceProvider clientParameters={{ auth: "/api/roomservice" }}>
       <Component {...pageProps} />
